9860dfe922651fa528489f356e84b87bf2af4d78
leejy <jiye.lee@ppbstudios.com>
2023-10-06 19:15:43 +0900

    인라인 javascript 처리기 구현

--- simpleJavascript.js
+++ simpleJavascript.js
@@ -6,3 +6,9 @@ function updateName() {
     const name = prompt("Enter a new name");
     para.textContent = `Player 1: ${name}`;
 }
+
+function createParagraph() {
+    const para = document.createElement("p");
+    para.textContent = "You clicked the button!";
+    document.body.appendChild(para);
+}
