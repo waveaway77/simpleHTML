3ed7698ab9b9746e95df1c538a7d1961094960d5
leejy <jiye.lee@ppbstudios.com>
2023-10-06 19:20:25 +0900

    인라인 javascript 삭제 후 addEventListener로 대체

--- simpleJavascript.js
+++ simpleJavascript.js
@@ -1,5 +1,6 @@
 document.querySelector("#spanId").textContent = "123";
 
+/* 텍스트 변경 버튼 */
 const para = document.querySelector("p");
 para.addEventListener("click", updateName);
 function updateName() {
@@ -7,8 +8,16 @@ function updateName() {
     para.textContent = `Player 1: ${name}`;
 }
 
+/* 문단 생성 버튼 */
+// createElement | appendChild
 function createParagraph() {
     const para = document.createElement("p");
     para.textContent = "You clicked the button!";
     document.body.appendChild(para);
+}
+
+// querySelectorAll | addEventListener
+const buttons = document.querySelectorAll("button");
+for (const button of buttons) {
+    button.addEventListener("click", createParagraph);
 }
