4318a42acba26576c182966d19473220b9c10447
leejy <jiye.lee@ppbstudios.com>
2023-10-06 18:49:08 +0900

    동적 버튼 구현

--- simpleJavascript.js
+++ simpleJavascript.js
@@ -1,1 +1,8 @@
 document.querySelector("#spanId").textContent = "123";
+
+const para = document.querySelector("p");
+para.addEventListener("click", updateName);
+function updateName() {
+    const name = prompt("Enter a new name");
+    para.textContent = `Player 1: ${name}`;
+}
